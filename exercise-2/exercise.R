471f27a02593c4181a5121cc12738bce7ab4c850
mariamatlick <mmatlick@uw.edu>
2019-04-29 17:08:54 -0700

    finished exercises in lab

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,26 +4,33 @@
 ## You should have have access to the `vehicles` data.frame
 
 ## Install (if you haven't already) and load the `dplyr`package
-
+## install.packages("dplyr")
+library(dplyr)
 # You should have have access to the `vehicles` data.frame
-
-
+##install.packages("fueleconomy")
+library(fueleconomy)
+View(vehicles)
 # Create a data.frame of vehicles from 1997
-
+cars1997 <- filter(vehicles, year== 1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(cars1997$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+two_wheel <- filter(vehicles, drive == "2-Wheel Drive", cty  > 20 )
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+filtered <- filter(two_wheel, hwy == min(hwy))
+worst_hwy <- select(filtered, id)
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
-
+make_year_filter <- function(my_make, my_year) {
+  filtered <- filter(vehicles, make == my_make, year == my_year) %>% filter(hwy == max(hwy))
+  filtered
+}
 
 # What was the most efficient honda model of 1995?
+df <- make_year_filter("Honda", 1995)
 
 
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -2,20 +2,32 @@
 
 # Install the nycflights13 package and read it in. Require the dplyr package.
 # install.packages("nycflights13")
+## install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
-
+View(flights)
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+ avg_delay <- flights %>% 
+   group_by(month) %>% 
+   summarise(delay = mean(dep_delay,  na.rm=TRUE))
 
 # If you create a data.frame with the columns "month", and "delay" above, you should be able to create 
 # a scatterplot by passing it to the 'plot' function
-
+lplot <- plot(avg_delay)
 
 # In which airport were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+which_airport <- flights %>% 
+  group_by(dest) %>% 
+  summarise(delay = mean(arr_delay, na.rm=TRUE)) %>% 
+  arrange(-delay)
 
 ### Bonus ###
 # Which city was flown to with the highest average speed?
+which_city <- flights %>% 
+  mutate(speed = distance/air_time * 60) %>% 
+  group_by(dest) %>% 
+  summarise(avg_speed = mean(speed, na.rm = TRUE)) %>% 
+  arrange(-avg_speed) 
+## Anchorage
